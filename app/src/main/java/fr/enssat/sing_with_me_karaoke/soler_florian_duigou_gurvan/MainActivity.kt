3701af18d0f62b6ef4f2b89940a42357ff875110
gduigou <gduigou@enssat.fr>
2024-12-09 11:54:24 +0100

    feat: add json to list

--- app/src/main/java/fr/enssat/sing_with_me_karaoke/soler_florian_duigou_gurvan/MainActivity.kt
+++ app/src/main/java/fr/enssat/sing_with_me_karaoke/soler_florian_duigou_gurvan/MainActivity.kt
@@ -7,15 +7,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import fr.enssat.sing_with_me_karaoke.soler_florian_duigou_gurvan.base.ExploreSongsScreen
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import fr.enssat.sing_with_me_karaoke.soler_florian_duigou_gurvan.model.AppDatabase
 import fr.enssat.sing_with_me_karaoke.soler_florian_duigou_gurvan.model.entities.Song
 import fr.enssat.sing_with_me_karaoke.soler_florian_duigou_gurvan.network.API
 import fr.enssat.sing_with_me_karaoke.soler_florian_duigou_gurvan.ui.theme.SingWithMeKaraokeTheme
@@ -23,29 +19,24 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+var songs: List<Song> = emptyList()
+
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        fetchSongs()
         setContent {
             SingWithMeKaraokeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
-                    ExploreSongsScreen(onSongClicked = { song ->
+                    ExploreSongsScreen(songList = songs, onSongClicked = { song ->
                         // Handle song click
-                        println("Song clicked: ${song.name}")
+                        Log.d("songs", "CLICKEDDDDDDDDDDDDDDDDDDDDDD")
                     })
-                    LoadSongsButton(onClick = { fetchSongs() })
                 }
             }
         }
-    }
-}
-
-@Composable
-fun LoadSongsButton(onClick: () -> Unit) {
-    Button(onClick = { onClick() }) {
-        Text("Load Songs")
     }
 }
 
@@ -53,8 +44,9 @@ fun fetchSongs() {
     API.restrofitService.getSongs().enqueue(object : Callback<String> {
         override fun onResponse(call: Call<String>, response: Response<String>) {
             if (response.isSuccessful) {
-                if (response.body() != null) {
-                    val songs = parseJson(response.body()!!) }
+                response.body()?.let {
+                    songs = parseJson(it)
+                }
             } else {
                 Log.d("songs", response.code().toString())
             }
